adcf96e08db4985daffa6536fc8019154c409af2
Jing Chan <jingchan@gmail.com>
2014-12-21 09:56:35 -0500

    Add toggle to debug window

--- main.js
+++ main.js
@@ -4,7 +4,12 @@
 
 'use strict';
 var $ = require('jquery');
+var config = require('./lib/config');
 
 // start app
 var app = require('./lib')();
 app.initialize('gameDiv');
+
+if(config.debug){
+	$('.debug_container').css('visibility', 'visible');
+}
